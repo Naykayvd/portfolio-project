ac2cf0393d543143ded52a2666f67583faf2c45f
NayKayvd <vandiemennahum@gmail.com>
2021-05-14 14:22:45 +0200

    updated

--- animation.js
+++ animation.js
@@ -1,3 +1,8 @@
 function toggleNavbar() {
   document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
 }
+function toggleNav() {
+  document
+    .getElementsByClassName("navbar-container")[0]
+    .classList.toggle("active");
+}
